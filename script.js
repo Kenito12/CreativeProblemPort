1b81a7a443170fff48275cf92033d967ec70e2fc
Ken <nontsuke.ken@gmail.com>
2024-02-18 23:59:18 -0500

    landing page done

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+console.log(document.URL)
+
 const loadingPage = document.getElementById("loaderWrappers");
 const landingPage = document.getElementById("landingpage");
 
 landingPage.style.display = 'none'
+
+const unitButton1 = document.getElementById("UnitButton1")
+const unitButton2 = document.getElementById("UnitButton2")
+const unitButton3 = document.getElementById("UnitButton3")
+const unitButton4 = document.getElementById("UnitButton4")
 
 
 setTimeout(() => {
@@ -12,3 +19,16 @@ setTimeout(() => {
     loadingPage.style.display = "none"
     landingPage.style.display = "block"
 }, 2800)
+
+unitButton1.onclick = () => {
+    window.location.href = "Unit1.html";
+}
+unitButton2.onclick = () => {
+    window.location.href = "index.html";
+}
+unitButton3.onclick = () => {
+    window.location.href = "index.html";
+}
+unitButton4.onclick = () => {
+    window.location.href = "index.html";
+}
