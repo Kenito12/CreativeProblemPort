ffd65da5d81ee6bad4c29bc38ed35ce2a83d6741
Ken <nontsuke.ken@gmail.com>
2024-02-20 11:56:29 -0500

    adding unit1page working on responsive text

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ const unit4Page = document.getElementById("unit4Page");
 //Set page default 
 loadingPage.style.display = 'none'
 landingPage.style.display = 'none'
+unit1Page.style.display = 'block'
+unit2Page.style.display = 'none'
+unit3Page.style.display = 'none'
+unit4Page.style.display = 'none'
 
 // Landing Page button
 const unitButton1 = document.getElementById("UnitButton1")
@@ -24,10 +28,10 @@ setTimeout(() => {
     loadingPage.style.animation = "LoadingAnimFadeOut ease-in-out 0.3s"
     // loadingPage.style.display = "none"
 }, 2500)
-setTimeout(() => {
-    loadingPage.style.display = "none"
-    landingPage.style.display = "block"
-}, 2800)
+// setTimeout(() => {
+//     loadingPage.style.display = "none"
+//     landingPage.style.display = "block"
+// }, 2800)
 
 unitButton1.onclick = () => {
     window.location.href = "Unit1.html";
