13c02d752d79d2fd5d48279e55710cc48c78facd
Ken <nontsuke.ken@gmail.com>
2024-03-31 22:36:47 -0400

    Unit 5 update

--- script.js
+++ script.js
@@ -7,41 +7,79 @@ const unit1Page = document.getElementById("unit1Page");
 const unit2Page = document.getElementById("unit2Page");
 const unit3Page = document.getElementById("unit3Page");
 const unit4Page = document.getElementById("unit4Page");
+const unit5Page = document.getElementById("unit5Page");
 
 
 //Set page default 
-loadingPage.style.display = 'none'
+loadingPage.style.display = 'block'
 landingPage.style.display = 'none'
-unit1Page.style.display = 'block'
+unit1Page.style.display = 'none'
 unit2Page.style.display = 'none'
 unit3Page.style.display = 'none'
 unit4Page.style.display = 'none'
+unit5Page.style.display = 'none'
 
 // Landing Page button
 const unitButton1 = document.getElementById("UnitButton1")
 const unitButton2 = document.getElementById("UnitButton2")
 const unitButton3 = document.getElementById("UnitButton3")
 const unitButton4 = document.getElementById("UnitButton4")
+const unitButton5 = document.getElementById("UnitButton5")
+const logoBut = document.querySelectorAll('.Unitlogo')
 
 
 setTimeout(() => {
     loadingPage.style.animation = "LoadingAnimFadeOut ease-in-out 0.3s"
-    // loadingPage.style.display = "none"
 }, 2500)
-// setTimeout(() => {
-//     loadingPage.style.display = "none"
-//     landingPage.style.display = "block"
-// }, 2800)
+setTimeout(() => {
+    loadingPage.style.display = "none"
+    landingPage.style.display = "block"
+}, 2800)
+
+logoBut.forEach((i) => {
+    i.onclick = () =>{
+        landingPage.style.display = 'block'
+        unit1Page.style.display = 'none'
+        unit2Page.style.display = 'none'
+        unit3Page.style.display = 'none'
+        unit4Page.style.display = 'none'
+        unit5Page.style.display = 'none'
+    }
+})
 
 unitButton1.onclick = () => {
-    window.location.href = "Unit1.html";
+    landingPage.style.animation = "LoadingAnimFadeOut ease-in-out 0.3s"
+    setTimeout(() => {
+        landingPage.style.display = 'none'
+        unit1Page.style.display = 'block';
+    }, 300)
 }
 unitButton2.onclick = () => {
-    window.location.href = "index.html";
+    landingPage.style.animation = "LoadingAnimFadeOut ease-in-out 0.3s"
+    setTimeout(() => {
+        landingPage.style.display = 'none'
+        unit2Page.style.display = 'block';
+    }, 300)
 }
 unitButton3.onclick = () => {
-    window.location.href = "index.html";
+    landingPage.style.animation = "LoadingAnimFadeOut ease-in-out 0.3s"
+    setTimeout(() => {
+        landingPage.style.display = 'none'
+        unit3Page.style.display = 'block';
+    }, 300)
 }
 unitButton4.onclick = () => {
-    window.location.href = "index.html";
+    landingPage.style.animation = "LoadingAnimFadeOut ease-in-out 0.3s"
+    setTimeout(() => {
+        landingPage.style.display = 'none'
+        unit4Page.style.display = 'block';
+    }, 300)
+}
+
+unitButton5.onclick = () => {
+    landingPage.style.animation = "LoadingAnimFadeOut ease-in-out 0.3s"
+    setTimeout(() => {
+        landingPage.style.display = 'none'
+        unit5Page.style.display = 'block';
+    }, 300)
 }
